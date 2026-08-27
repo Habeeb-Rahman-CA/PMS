7f0012e44414f0472f36df87455d7845d4c0ca32
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2026-08-27 18:51:46 +0530

    feat: add MyWork feature and integrate it into application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProjectsComponent } from './features/projects/projects';
 import { TasksComponent } from './features/tasks/tasks';
+import { MyWorkComponent } from './features/my-work/my-work';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,10 @@ export const routes: Routes = [
   {
     path: 'tasks',
     component: TasksComponent
+  },
+  {
+    path: 'my-work',
+    component: MyWorkComponent
   },
   {
     path: '**',
