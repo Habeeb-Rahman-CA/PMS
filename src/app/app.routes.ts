e2e0f19543b0d7a394ff9326ccac9718e9c80b5f
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2026-08-27 22:54:10 +0530

    refactor: remove My Work feature component and clean up related route and layout references

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { ProjectsComponent } from './features/projects/projects';
 import { TasksComponent } from './features/tasks/tasks';
-import { MyWorkComponent } from './features/my-work/my-work';
 
 export const routes: Routes = [
   {
@@ -11,10 +10,6 @@ export const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent
-  },
-  {
-    path: 'my-work',
-    component: MyWorkComponent
   },
   {
     path: 'tasks',
